d3a1d0bdd3b9a04b33bd3e1bd0bf21010fc881c5
QLisaL <qingyili@usc.edu>
2024-09-06 12:54:27 -0700

    Finished Week 02 Homework
    Finished Week 02 Homework

--- hw02/EnemyCollision.cs
+++ hw02/EnemyCollision.cs
@@ -6,36 +6,48 @@ public class EnemyCollision : MonoBehaviour
 {
     // Start is called before the first frame update\
     //blah blah
+
+    Weapon myWeapon = new Weapon();
     void Start()
     {
-        
+        myWeapon.currentWeapon = myWeapon.sword;
+
+        StartCoroutine(switchWeapon());
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
     // TODO: Problem 7: Why did I not include the words public or private here?
+    // Answer: Because when you don't include is set to private by default. 
     // What does it mean when I only write void (Movement)? What does void mean?
-    void Movement ()
+    // Answer: void means you don't want things to return after the code finish running.
+
+
+
+    void Movement()
     {
         // TODO: Problem 8: Please explain what the next 4 lines mean.
         // What is Input.GetAxis, transform.Translate, and transform.Rotate? 
-        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
-        
-        transform.Translate(Vector3.forward * forwardMovement);
-        transform.Rotate(Vector3.up * turnMovement);
+        // Answer: The Input.GetAxis means get the input axis. The transform.Translate means to move the gameObject with certain direction's distance. The transform.Rotate means to get the rotate gameObject with certain rotation value.
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime; // Is to set the forwardMovement in Unity, by getting the Vertical axis input to times the speed and the time per frame.
+        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;// Is to set the turnMovement in Unity, by getting the Horizontal axis input to rotations times the speed and the time per frame.
+
+        transform.Translate(Vector3.forward * forwardMovement); // this is the function actually move the gameObject.
+        transform.Rotate(Vector3.up * turnMovement); // This is the function actually rotate the gameObject.
     }
-    
-    
-    void Shoot ()
+
+
+    void Shoot()
     {
         // TODO: Problem 9: Looking at this code, is this code using Unity's Old or New 
         // Input System? Please describe what Instantiate is doing in this if statement.
-        if(Input.GetButtonDown("Fire1") && myStuff.bullets > 0)
+        // Answer: This is Unity Old, because the Unity New will use InputAction. Instantiate is create a prefab instance.
+        if (Input.GetButtonDown("Fire1") && myStuff.bullets > 0)
         {
             Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
             bulletInstance.AddForce(firePosition.forward * bulletSpeed);
@@ -43,6 +55,24 @@ public class EnemyCollision : MonoBehaviour
         }
     }
 
+    private IEnumerator switchWeapon()//is for executing a sequence of events
+    {
+        while (true)
+        {
+            myWeapon.currentWeapon = myWeapon.arrow;
+
+            yield return new WaitForSeconds(5f);
+
+            myWeapon.currentWeapon = myWeapon.sword;
+
+            yield return new WaitForSeconds(5f);
+
+            myWeapon.currentWeapon = myWeapon.rocket;
+
+            yield return new WaitForSeconds(5f);
+        }
+
+    }
     /* TODO: Problem 10: In our game, we want our enemy to alternate weapons every couple of frames.
     However, also would like our enemy to lose access to their weapons when they are frozen/disabled.
     First, let's define a public class named Weapon. In the class, define 3 int variables for arrow, sword, and rocket.
@@ -53,5 +83,15 @@ public class EnemyCollision : MonoBehaviour
     Use the WaitForSeconds function to tell it to switch weapons every 5 seconds. 
 
     Remember to call your Coroutine.
-    */ 
+    */
+}
+
+public class Weapon
+{
+    public int arrow = 0;
+    public int sword = 1;
+    public int rocket = 2;
+
+    public int currentWeapon;
+
 }
--- hw02/PlayerController.cs
+++ hw02/PlayerController.cs
@@ -1,11 +1,15 @@
 // TODO: Problem 1: Write in the comments what each line of code is doing.
 // What is "using" and what is System.Collections? Please provide comments on what the next three lines mean. 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// by inlcuding “using“ with different namespaces after it, you can use those functions, classes, functions, or other organized logically by just calling their namesapce in this script, and the conputer will know what todo.
+using System.Collections; // This namesapce included lots of useful things like lists, queues, bit arrays, hash tables, and dictionaries.
+using System.Collections.Generic; // Is a Generic collection, which allow me to use things like compare<T> and Dictionary.
+using UnityEngine; // It contain core function namespaces in Unity Engine, like variable type, GameObject.
+using UnityEngine.InputSystem; // Is a namespace could help to user to use different ways like device, gestures, and touch to control the game obejct.
 
 public class PlayerController : MonoBehaviour
 {
+
+
     /* 
     TODO: Problem 2: We are trying to move our player in an open world game. We would like the player to be able to move
     foward, backward, left, and right. In addition, the player should be able to jump and adapt to the world's gravity.
@@ -18,34 +22,65 @@ public class PlayerController : MonoBehaviour
     
     When the game starts, we would like to find our character. In our Unity Editor, we have a 3D model of our player and the player is 
     represented as a component in Unity. How do we grab our player controller in code and where should we write this line of code in this
-    C# document (PlayerController.cs)?
+    C# document (PlayerController.cs)? 
     */
-    
+
+    public string characterName;
+    public float speed;
+    public float gravityValue;
+    public float jumpSpeed;
+    public PlayerController myPlayerController;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        // Answer: myPlayerController = GameObject.Find("Player").GetComponent<PlayerController>();
         // TODO: Problem 3: Describe when you would put code in the Start() function instead of the Update() function. 
+        // Answer: The part of the game when you want the code to run when the game start but not want they update each frame.
         // What is the difference between Start() and Update()?
+        // Answer: The Start() will only run when the game start, the Update() will run each frame during the game running to make sure everything update ontime.
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // TODO: Problem 4: What is an if statement? Write a couple of if statements to move the player with the WASD keys. 
+        // Answer: A if statement is you run the script only when the if condition satisfied.
         // We want to transform the player's position along the vertical and horizontal axis then multiply it by 
         // the corresponding variables we defined earlier. Lastly, we will need to multiply it all by Time.DeltaTime
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, 5) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -5) * Time.deltaTime);
+        }
+        //Debug.Log(Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-5, 0, 0) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(5, 0, 0) * Time.deltaTime);
+        }
     }
 
     // TODO: Problem 5: What do the lines here do? Please provide inline comments and describe what each line is doing
     // What do Header, SerializeField, and Tooltip mean? 
-    [Header("General Setup Settings")]
-    [SerializeField] private InputAction movement;
-    [Tooltip("How fast player moves up and down based upon player input")][SerializeField] float controlSpeed = 30f;
-    [Tooltip("How far player moves horizontally")][SerializeField] float xRange = 10f;
-    [Tooltip("How far player moves vertically")][SerializeField] float yRange = 10f;
+    [Header("General Setup Settings")] // In the Inspector add UI with Header looking style, to help increase the readibility.
+    [SerializeField] private InputAction movement; // To make private variable to displayable and editable in Unity Inspector.
+    [Tooltip("How fast player moves up and down based upon player input")][SerializeField] float controlSpeed = 30f; // When your mouse hover on the variable controlSpeed, it will show the tip.
+    [Tooltip("How far player moves horizontally")][SerializeField] float xRange = 10f; // When your mouse hover on the variable xRange, it will show the tip.
+    [Tooltip("How far player moves vertically")][SerializeField] float yRange = 10f; // When your mouse hover on the variable yRange, it will show the tip.
 
     // TODO: Problem 6: What are Unity's OnEnable() and OnDisable functions?
+    // OnEnable() is activate the input action and OnDisable is diactivate the input action
     // What do movement.Enable() and movement.OnDisable do?
+    // movement.Enable() will enable the movement input action, the movement.OnDisable will stop the movement input action.
     private void OnEnable()
     {
         movement.Enable();
