42c28da51a2d68a8005b22fff1dab0a3b8052bd1
Debbie Yuen <deborahyuen2016@berkeley.edu>
2024-09-15 22:14:48 -0700

    hw04 completed homework problems

--- hw04/BarbieWalletBalance.cs
+++ hw04/BarbieWalletBalance.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BarbieWalletBalance : MonoBehaviour
+public class BarbieBank : MonoBehaviour
 {
 
     // Start is called before the first frame update
+
+    /* TODO: Problem 3: BARBIE'S BANK
+    Convert the following function to a generic if needed. 
+    Then, write a private generic function named BarbieBank. 
+    BarbieBank should take in the parameters: numOfPennies, cashAmount, and numOfCreditCards
+    Have the function return a new generic array with the correct parameters. 
+    */
+
     void Start()
     {
-        int[] biggerWallet = BarbieBank(500, 600);
+        int[] biggerWallet = BarbieWallet(500, 600);
         Debug.Log(biggerWallet.Length + " " + biggerWallet[0] + " " + biggerWallet[1]);
 
         GetMoney(500, 600);
     }
 
-    // TODO: Problem 1: Barbie's Wallet
-    // Convert the following GetMoney( ) function to a generic.
-    private int[] BarbieBank(int numOfPennies, int cashAmount, int numOfCreditCards) {
-        return new int[] {numOfPennies, cashAmount, numOfCreditCards};
-    }
-
-    // TODO: Problem 2: Ken's Wallet
-    //  What if we need more money? Barbies Bank went out. 
-    private void KenBank<T1, T2>(T1 money, T2 moreMoney) {
+    /* TODO: Problem 4: INHERITANCE: SHORT ANSWERS
+        * What is the "Protected" access modifier? How does it relate to inheritence and between two classes. 
+        * What is MonoBehaviour? Why do Unity C# scripts inherit from MonoBehaviour? Give some examples of Unity functions that come from MonoBehaviour. 
+        * What is multiple inheritance? Can we perform multiple inheritance in C#? Why or why not?
+        * What does "Protected virtual void" mean? When is it needed and what does virtual do in your code?
+        * What happens when a constructor in a parent class is called? How do we control which base class contructor is being called?
+     */ 
+    private void KenWallet<T1, T2>(T1 money, T2 moreMoney) {
         Debug.Log(money.GetType());
         Debug.Log(moreMoney.GetType());
     }
