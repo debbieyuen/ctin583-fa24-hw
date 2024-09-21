38e0efb059245e904707b408daf9389d708d8d79
AriaQin210 <139326652+AriaQin210@users.noreply.github.com>
2024-09-20 23:30:30 -0700

    Update BarbiesHouse.cs

--- hw04/BarbiesHouse.cs
+++ hw04/BarbiesHouse.cs
@@ -10,5 +10,20 @@ furniture, pets, household items, food, etc. within her house.
 */ 
 public class BarbieHouse : MonoBehaviour
 {
-    
+     public void DisplayFurniture()
+    {
+        Debug.Log("Barbie has a couch and a bed.");
+    }
+
+    // Method to represent Barbie's pets
+    public void ShowPets()
+    {
+        Debug.Log("Barbie has a dog.");
+    }
+
+    // Method to represent food items in Barbie's kitchen
+    public void ShowFood()
+    {
+        Debug.Log("Barbie's fridge is stocked with salads.");
+    }
 }
