258bcf9e8f5ba0fb829113919f7c6842e3c28016
AriaQin210 <139326652+AriaQin210@users.noreply.github.com>
2024-10-09 11:21:53 -0700

    Update BarbieBirthday.cs

--- hw07/BarbieBirthday.cs
+++ hw07/BarbieBirthday.cs
@@ -34,13 +34,112 @@ public class BarbieBirthday : MonoBehaviour
     [SerializeField] ParticleSystem candyExplosion;
 
     // Barbie's height is Barbie is 5'9'' or 1.75 meters tall
-    private Vector2 barbieHeight;
+    private Vector2 barbieHeight = new Vector2(0,1.75f);
 
     // Barbie is standing 2 feet or 0.6 meters away from the her party pinata
-    private Vector2 barbieToPinataDistance;
+    private Vector2 barbieToPinataDistance = new Vector2(0.6f, 0);
 
     // The party pinata is hanging 8.2 feet or 2.5 meters high from the ground
-    private Vector2 pinataHeight;
+    private Vector2 pinataHeight  new Vector2(0,2.5f);
+
+   //Solve problem 1
+    private int swingsRemaining = 3;
+    private float requiredSwingMagnitude;
+    private int successfulHits = 0;
+
+    private void Start()
+    {
+        CalculateRequiredSwingMagnitude();
+    }
+    private void CalculateRequiredSwingMagnitude()
+    {
+        float heightDifference = pinataHeight.y - barbieHeight.y;
+        requiredSwingMagnitude = Mathf.Sqrt(Mathf.Pow(heightDifference,2) +  Mathf.Pow(barbieToPinataDistance.x,2));
+    }
+    public void Swing(float swingMagnitude)
+    {
+        if(swingsRemaining<=0)
+        {
+            Debug.Log("Barbie has run out three chances to swing at the pinata");
+            return;
+        }
+           
+        swingsRemaining--;
+    }
+
+     //Solve problem 2
+        private bool IsFacingPinate()
+        {
+        Vector2 barbieToPinata = new Vector2(pinata.transform.position.x-transform.position.x, pinata.transform.position.y-transform.position.y);
+            float angle = Vector2.Angle(transform.right, barbieToPinata);
+            return angle < 5f;
+      
+        }
+        private void HandSuccessfulHit()
+        {
+            successfulHits++;
+
+            switch (successfulHits)
+            {
+                case 1:
+                    pinata.rotation  =  rotationX;
+                    break;
+                case 2:
+                    Quaternion combinedRotation = rotationX * rotationY;
+                    pinata.rotation = combinedRotation;
+                    CompareQuaternions(rotationX, rotationY);
+                    break;
+                case 3:
+                    candyExplosion.Play();
+                
+                    break;
+
+            }
+
+        }
+       //Solve problem 2
+        private bool IsFacingPinate()
+        {
+        Vector2 barbieToPinata = new Vector2(pinata.transform.position.x-transform.position.x, pinata.transform.position.y-transform.position.y);
+            float angle = Vector2.Angle(transform.right, barbieToPinata);
+            return angle < 5f;
+      
+        }
+        private void HandSuccessfulHit()
+        {
+            successfulHits++;
+
+            switch (successfulHits)
+            {
+                case 1:
+                    pinata.rotation  =  rotationX;
+                    break;
+                case 2:
+                    Quaternion combinedRotation = rotationX * rotationY;
+                    pinata.rotation = combinedRotation;
+                    CompareQuaternions(rotationX, rotationY);
+                    break;
+                case 3:
+                    candyExplosion.Play();
+                
+                    break;
+
+            }
+
+        }
+         private void CompareQuaternions(Quaternion q1, Quaternion q2)
+            {
+                if (Quaternion.Angle(q1, q2) < 0.1f)
+                {
+                    Debug.Log("rotationX and rotationY are Equal");
+                }
+                else
+                {
+                    Debug.Log("Quaternions are different");
+                }
+             }
+
+
 
     // Update is called once per frame
     void Update()
@@ -53,6 +152,10 @@ public class BarbieBirthday : MonoBehaviour
         */      
         Vector2 relativePosition = pinata.position - pinata.position;
 
+        if (IsFacingPinate())
+        {
+            Debug.Log("Barbie is facing the pinata");
+        }
         /*
         TODO: Problem 3: Barbie swings her bat and the bat hits the pinata. The pinata is now rotating along the x axis.
         If Barbie hits the bat again, we want the pinata to spin along the x axis and the y axis. How do we get the correct overall rotation of the pinata?
@@ -61,5 +164,12 @@ public class BarbieBirthday : MonoBehaviour
             * Hint: Check if two Quaternions are equal to each other. If they are, print out "[Names of Quaterions] are Equal". Else, print out "Quaternions are different"
             * Hint: How can we rotate our vector? Can we use Quaternion.Lerp and Quaternion.Slerp?
         */
+        //Solve problem 3
+
+        if (successfulHits > 0 && successfulHits < 3)
+        {
+            Quaternion targetRotation = (successfulHits == 1) ? rotationX : (rotationX * rotationY);
+            pinata.rotation = Quaternion.Slerp(pinata.rotation, targetRotation, Time.deltaTime);
+        }
     }
 }
