24943139b170b66f90254ee8c318a1cf2ac546a9
JelaniMcKenzie <jelani.p.mckenzie@gmail.com>
2024-10-20 00:45:22 -0700

    Finished HW07

--- hw07/BarbieBirthday.cs
+++ hw07/BarbieBirthday.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 /* 
@@ -14,11 +16,10 @@ Barbie is 5'9'' or 1.75 meters tall.
 She is standing 2 feet or 0.6 meters away from the her party pinata.
 The party pinata is hanging 8.2 feet or 2.5 meters high from the ground.
 Assume that the angles between the distance of the pinata to the ground and Barbie's distance to the pinata create a 90 degree angle. 
-
 TODO: Problem 1: Barbie is holding a bat to swing at the pinata. What should be the magnitude the bat should swing at? Make sure the check for edge cases including:
      * Barbie has only three chances to swing at the pinata before it is the next player's turn
      * If Barbie runs out of turns, a message should display that Barbie's turn is over and it is the next player's turn
-*/ 
+*/
 public class BarbieBirthday : MonoBehaviour
 {
     // Transform for Barbie's party pinata
@@ -34,13 +35,18 @@ public class BarbieBirthday : MonoBehaviour
     [SerializeField] ParticleSystem candyExplosion;
 
     // Barbie's height is Barbie is 5'9'' or 1.75 meters tall
-    private Vector2 barbieHeight;
+    private Vector2 barbieHeight = new Vector2(0, 1.75f);
 
     // Barbie is standing 2 feet or 0.6 meters away from the her party pinata
-    private Vector2 barbieToPinataDistance;
+    private Vector2 barbieToPinataDistance = new Vector2(0.6f, 0);
 
     // The party pinata is hanging 8.2 feet or 2.5 meters high from the ground
-    private Vector2 pinataHeight;
+    private Vector2 pinataHeight = new Vector2(0, 2.5f);
+    private int attempts = 3;
+
+    private float swingMagnitude = 0f;
+
+    private Quaternion currentRotation = Quaternion.identity;
 
     // Update is called once per frame
     void Update()
@@ -50,7 +56,23 @@ public class BarbieBirthday : MonoBehaviour
         Check to make sure that Barbie is facing directly at the pinata. 
         * Hint: The target should be the pinata
         * Hint: Make the object points towards the object. Look at returns a quaterion and takes in a vector
-        */      
+        */
+
+
+        if (transform.forward == pinata.position) 
+        {
+            if (Input.GetMouseButtonDown(0)) 
+            {
+                Swing();
+                RotatePinata(currentRotation, currentRotation, attempts);
+            }
+        }
+        else 
+        {
+            transform.LookAt(pinata.position);
+            return;
+        }
+
         Vector2 relativePosition = pinata.position - pinata.position;
 
         /*
@@ -61,5 +83,61 @@ public class BarbieBirthday : MonoBehaviour
             * Hint: Check if two Quaternions are equal to each other. If they are, print out "[Names of Quaterions] are Equal". Else, print out "Quaternions are different"
             * Hint: How can we rotate our vector? Can we use Quaternion.Lerp and Quaternion.Slerp?
         */
+    }
+
+    private void Swing() 
+    {
+        if (attempts <= 0) 
+        {
+            Debug.Log("Out of attempts! Next player's turn");
+            return;
+        }
+
+        //form a right triangle to calculate swing distance
+        Vector2 heightDifference = new Vector2 (0, pinataHeight.y - barbieHeight.y);
+
+        //the hypotenuse represents the swing path
+        swingMagnitude = Mathf.Sqrt(Mathf.Pow(heightDifference.y, 2) + Mathf.Pow(barbieToPinataDistance.x, 2));
+        attempts--;
+
+    }
+
+    //Problem 3 solution
+    private void RotatePinata(Quaternion xRotation, Quaternion yRotation, int attemptNum) 
+    {
+        Quaternion defaultRotation = Quaternion.identity;
+        xRotation = Quaternion.identity;
+        yRotation = Quaternion.identity;
+
+        switch (attemptNum) {
+            case 2 : {
+                xRotation = Quaternion.Euler(45, 0, 0);
+                pinata.rotation = xRotation;
+                Quaternion.Slerp(defaultRotation, xRotation, Time.deltaTime);
+                break;
+            }
+            case 1 : {
+                yRotation = Quaternion.Euler(0, 45, 0);
+                pinata.rotation = rotationX * rotationY;
+                Quaternion.Slerp(defaultRotation, pinata.rotation, Time.deltaTime);
+
+                if (rotationX.Equals(rotationY)) {
+                    Debug.Log("Rotations are equal");
+                }
+                else 
+                {
+                    Debug.Log("Rotations are different");
+                }
+                break;
+            }
+            case 0 : {
+                candyExplosion.Play();
+                break;
+            }
+            default : {
+                Debug.Log("Pinata is broken!");
+                break;
+            }
+        }
     }
 }
