596d599b8404a047748bb2108183be7ccf71ae8c
Cheyenne Chau <ciaobellacheyenne@gmail.com>
2025-02-02 15:01:47 -0800

    submit hw assignment, add .igtignore

--- hw03/EnemyCollision.cs
+++ hw03/EnemyCollision.cs
@@ -7,7 +7,7 @@ public class EnemyCollision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SwitchWeapon());
     }
 
     // Update is called once per frame
@@ -18,15 +18,18 @@ public class EnemyCollision : MonoBehaviour
     
     // TODO: Problem 7: Why did I not include the words public or private here?
     // What does it mean when I only write void (Movement)? What does void mean?
-    void Movement ()
+    void Movement () // when no access modifier (public/private) is specified, it defaults to private; void means that the method does not return a value, just perform some sort of action(s)
     {
         // TODO: Problem 8: Please explain what the next 4 lines mean.
         // What is Input.GetAxis, transform.Translate, and transform.Rotate? 
-        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;  // gets input values between -1 and 1 from vertical axis (w/s or up/down keys)
+        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime; // gets input values between -1 and 1 from horizontal axis (a/d or left/right keys)
+        // Input.GetAxis reads a player input from a specified axis (Vertical, Horizontal, etc) and returns a float value between -1 and 1
         
-        transform.Translate(Vector3.forward * forwardMovement);
-        transform.Rotate(Vector3.up * turnMovement);
+        transform.Translate(Vector3.forward * forwardMovement); // moves object forward/backward based on input
+        transform.Rotate(Vector3.up * turnMovement); // rotates object left/right based on input
+        // transform.Translate moves an object in a given direction by a specified amount
+        // transform.Rotate rotates an object around specified axes
     }
     
     
@@ -34,9 +37,9 @@ public class EnemyCollision : MonoBehaviour
     {
         // TODO: Problem 9: Looking at this code, is this code using Unity's Old or New 
         // Input System? Please describe what Instantiate is doing in this if statement.
-        if(Input.GetButtonDown("Fire1") && myStuff.bullets > 0)
+        if(Input.GetButtonDown("Fire1") && myStuff.bullets > 0) // this is Unity's Old Input System since it uses Input.GetButtonDown(); new system uses InputAction and requires a different setup in PlayerInput
         {
-            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody; // here, Instantiate() create a new bullet at firePosition with the specified rotation, then casts it as a Rigidbody
             bulletInstance.AddForce(firePosition.forward * bulletSpeed);
             myStuff.bullets--;
         }
@@ -53,4 +56,28 @@ public class EnemyCollision : MonoBehaviour
 
     Remember to call your Coroutine.
     */ 
+
+    public class Weapon { // arbitrary values of each weapon
+        public int arrow = 14;
+        public int sword = 7;
+        public int rocket = 3;
+    }
+
+    private int currentWeapon; // keep track of enemy's current weapon
+    private IEnumerator SwitchWeapon () {
+        Weapon weapons = new Weapon();
+
+        while(enabled) { // only when enemy is active
+
+        // change out weapons every 5 seconds
+        currentWeapon = weapons.arrow;
+        yield return new WaitForSeconds(5);
+
+        currentWeapon = weapons.sword;
+        yield return new WaitForSeconds(5);
+
+        currentWeapon = weapons.rocket;
+        yield return new WaitForSeconds(5);
+        }
+    }
 }
--- hw03/PlayerController.cs
+++ hw03/PlayerController.cs
@@ -1,8 +1,10 @@
 // TODO: Problem 1: Write in the comments what each line of code is doing.
 // What is "using" and what is System.Collections? Please provide comments on what the next three lines mean. 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+
+// "using" allows us to access classes and methods from namespaces
+using System.Collections; // contains interfaces and classes for non-generic collections (ArrayList, Queue, etc)
+using System.Collections.Generic; // contains interfaces and classes for generic collections (List<T>, Dictionary<T, K>, etc) (generic collections are more type safe and do not require type casting)
+using UnityEngine; // contains classes specific to unity for game development (MonoBehavior, GameObject, etc)
 
 public class PlayerController : MonoBehaviour
 {
@@ -20,12 +22,37 @@ public class PlayerController : MonoBehaviour
     represented as a component in Unity. How do we grab our player controller in code and where should we write this line of code in this
     C# document (PlayerController.cs)?
     */
+
+    // private variables
+    private string playerName = "Character";
+    private float movementSpeed = 5.0f;
+    private float gravityValue = -9.81f;
+    private float jumpSpeed = 8.0f;
+
+    // declare a variable of type CharacterController
+        private CharacterController controller;
     
     // Start is called before the first frame update
     void Start()
     {
+        // run once, put it at beginning of Start() to ensure all components are ready
+        controller = GetComponent<CharacterCOntroller>
+
+        // print out to Unity's console
+        Debug.Log("Player Name: " + playerName);
+        Debug.Log("Movement Speed: " + movementSpeed);
+        Debug.Log("Gravity Value: " + gravityValue);
+        Debug.Log("Jump Speed: " + jumpSpeed);
+
         // TODO: Problem 3: Describe when you would put code in the Start() function instead of the Update() function. 
         // What is the difference between Start() and Update()?
+
+        // Put code in Start() when initializing variables, getting components, setting up initial game state, etc
+        // This is because Start() is only called once when the script is first being executed
+
+        // Put code in Update() for any behavior/actions that need to be updated every frame, such as movement, physics, input, etc
+        // This is because Update() is called every frame
+
     }
 
     // Update is called once per frame
@@ -34,25 +61,42 @@ public class PlayerController : MonoBehaviour
         // TODO: Problem 4: What is an if statement? Write a couple of if statements to move the player with the WASD keys. 
         // We want to transform the player's position along the vertical and horizontal axis then multiply it by 
         // the corresponding variables we defined earlier. Lastly, we will need to multiply it all by Time.DeltaTime
+
+        // An if statement sections a piece of code with a condition; if the condition is satisfied, then the code will execute
+        float horizontalInput = Input.GetAxis("Horizontal"); // get input value (-1 to 1) for horizontal (a/d) movement
+        float verticalInput = Input.GetAxis("Vertical"); // get input value (-1 to 1) for vertical (w/s) movement
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput); // create movement vector by combining horizontal and vertical input into 3D vector
+
+        vector3 movementAmt = movement * movementSpeed * Time.deltaTime; // apply movement speed to movement and make it independent of frame-rate (for consistent speed)
+
+        controller.Move(movementAmt); // move controller, moves the character
+
+        if(verticalInput.GetKeyDown(KeyCode.Space && controller.isGrounded) ) { // checks for jump input; if space is pressed and character is on ground
+            movementAmt.y = jumpSpeed; // apply jump force
+        }
+
+        if(!controller.isGrounded) { // apply gravity; if character is in air
+            movementAmt.y += gravityValue * Time.deltaTime; // pull character down
+        }
     }
 
     // TODO: Problem 5: What do the lines here do? Please provide inline comments and describe what each line is doing
     // What do Header, SerializeField, and Tooltip mean? 
-    [Header("General Setup Settings")]
-    [SerializeField] private InputAction movement;
-    [Tooltip("How fast player moves up and down based upon player input")][SerializeField] float controlSpeed = 30f;
-    [Tooltip("How far player moves horizontally")][SerializeField] float xRange = 10f;
-    [Tooltip("How far player moves vertically")][SerializeField] float yRange = 10f;
+    [Header("General Setup Settings")] // creates a header in Unity Inspector to group variables for better oganization
+    [SerializeField] private InputAction movement; // makes private variables visible in Unity Inspector
+    [Tooltip("How fast player moves up and down based upon player input")][SerializeField] float controlSpeed = 30f;4    [Tooltip("How far player moves horizontally")][SerializeField] float xRange = 10f; // displays tooltip in Unity Inspector to explain what variable does
+    [Tooltip("How far player moves vertically")][SerializeField] float yRange = 10f; // displays tooltip in Unity Inspector to explain what variable does
 
     // TODO: Problem 6: What are Unity's OnEnable() and OnDisable functions?
     // What do movement.Enable() and movement.OnDisable do?
-    private void OnEnable()
+    private void OnEnable() // called when script is enabled or the GameObject is activated
     {
-        movement.Enable();
+        movement.Enable(); // enables input system to start receiving player inputs
     }
 
-    private void OnDisable()
+    private void OnDisable() // called when script is disabled or GameObject is activated
     {
-        movement.Disable();
+        movement.Disable(); // disables input system to stop receiving inputs when player is inactive
     }
 }
